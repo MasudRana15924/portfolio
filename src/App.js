63565a184d03b05345b59ed19505067e9c6272de
MasudRana15924 <masudrana15924@outlook.com>
2023-08-09 23:30:31 +0600

    logo added

--- src/App.js
+++ src/App.js
@@ -6,30 +6,31 @@ import {
 } from "react-router-dom";
 import Home from './pages/home/Home';
 import Navbar from './screen/shared/Navbar';
-import Projects from './pages/home/Projects';
 import Footer from './screen/shared/Footer';
-import Contact from './pages/home/Contact';
-import Education from './pages/home/Education';
-import Courses from './pages/home/Courses';
+import { useEffect, useState } from 'react';
+import Loader from './pages/loader/Loader';
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  })
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Routes>
-          <Route exact path="/" element={<Home />}>
-            
-          </Route>
-          <Route path="/home" element={<Home />} />
-          {/* <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/course" element={<Courses />} /> */}
-
-        </Routes>
-        <Footer></Footer>
-      </BrowserRouter>
-    </div>
+<div>
+    { loading ? <div>
+      <Loader></Loader >
+    </div > : <div className="App">
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
+  </div>}
+</div>
   );
 }
 
---
+++ src/pages/loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/masud-rana-logo.png'
+
+const Loader = () => {
+    return (
+        <div className="w-2/4 lg:w-1/4 mx-auto mt-64 lg:mt-72">
+            <img src={logo} alt="" className=""/>
+        </div>
+    );
+};
+
+export default Loader;
